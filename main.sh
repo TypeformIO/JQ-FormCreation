57470b6d48ae87d0d0e5e205d8c0e84716a3b394
Victor Bjelkholm <victor@typeform.com>
2015-09-09 09:05:12 +0200

    Update 0.1 -> 0.4 and change links

--- main.sh
+++ main.sh
@@ -3,6 +3,6 @@ for i in $(seq 0 $(jq 'length - 1' users.json)); do
   jq -r --arg i $i '.[$i | tonumber].email' users.json
   jq --arg i $i -f form.jq users.json | \
     curl -s -d @- -H "X-API-TOKEN: $1" \
-      https://api.typeform.io/v0.1/forms | \
-    jq -r '.links.form_render.get'
+      https://api.typeform.io/v0.4/forms | \
+    jq -r '._links[1].href'
 done
